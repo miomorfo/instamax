f4b693e7c774f3a8a771d06393c0cfda80c0fe02
Fernando <fernando.eb@icloud.com>
2020-11-15 14:34:40 -0300

    se añaden los controladores para las vistas de la aplicación

---
+++ instamax/Controllers/CameraViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CameraViewController.swift
+//  instamax
+//
+//  Created by Fernando on 15-11-20.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ instamax/Controllers/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  instamax
+//
+//  Created by Fernando on 15-11-20.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	
+}
