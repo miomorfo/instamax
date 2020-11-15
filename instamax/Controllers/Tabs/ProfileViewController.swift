c03527d7c94e338ae26e66a5b45c269250e86021
Fernando <fernando.eb@icloud.com>
2020-11-15 15:31:25 -0300

    se crean las Tabs principales y se les asignan Views y Controllers

---
+++ instamax/Controllers/Tabs/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  instamax
+//
+//  Created by Fernando on 15-11-20.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
